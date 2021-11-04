d441de5d694db7160a108916cf97fb9a1235c178
Marcus Gotling <marcus@gotling.se>
2021-11-04 13:57:26 +0100

    Add models to backend

--- backend/api/room/services/room.js
+++ backend/api/room/services/room.js
@@ -5,8 +5,4 @@
  * to customize this service
  */
 
-module.exports = {
-  find(params, populate) {
-    return strapi.query('stay').find(params, ["bed", "person", "room.house"]);
-  }
-};
+module.exports = {};
