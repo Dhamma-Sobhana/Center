d977923b978c8f2b35deaf610a3d44b26cbd93df
Marcus Gotling <marcus@gotling.se>
2022-05-20 11:39:25 +0200

    Start with empty Strapi 4 project

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'bb70435a53db074d7f60dde24c24fcd0'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
